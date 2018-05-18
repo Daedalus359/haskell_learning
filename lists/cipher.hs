4ffc16a191e2203d3f69f5c61a9a10f79857ba77
Kevin <kbradner112@gmail.com>
2018-05-17 22:17:39 -0400

    minor correction to Cipher

--- lists/cipher.hs
+++ lists/cipher.hs
@@ -4,8 +4,14 @@ module Cipher where
     mapChar :: Int -> Char -> Char
     mapChar n c = chr . (+n) . ord $ c
 
+    wrapChar :: Char -> Char
+    wrapChar c
+        |ord c < ord 'a' = wrapChar . chr $ (ord c + 26)
+        |ord c - ord 'a' < 26 = c
+        |otherwise = chr $ ord 'a' + (mod (ord c - ord 'a') 26)
+
     scramble :: Int -> String -> String
-    scramble n = map (mapChar n)
+    scramble n = map (wrapChar . mapChar n)
 
     caesar = scramble 3
 
