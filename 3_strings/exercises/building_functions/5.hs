e0cafd586818c3e1af8840b531c40983d0148fc8
= <=>
2018-10-05 12:40:51 -0700

    symlink ex6 to ex5

--- 3_strings/exercises/building_functions/5.hs
+++ 3_strings/exercises/building_functions/5.hs
@@ -1,2 +1,7 @@
-tdCIA :: String -> String
-tdCIA x = take 7 (drop 9 x) ++ take 4 (drop 5 x) ++ take 5 x
+module Reverse where
+
+rvrs :: String -> String
+rvrs x = take 7 (drop 9 x) ++ take 4 (drop 5 x) ++ take 5 x
+
+main :: IO ()
+main = print $ rvrs "Curry is awesome1"
