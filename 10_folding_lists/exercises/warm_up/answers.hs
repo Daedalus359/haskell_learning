34930c9adbc7ca9f653dff07c37955ce8f5fb2c4
= <=>
2018-10-09 11:27:52 -0700

    combos exercise

---
+++ 10_folding_lists/exercises/warm_up/answers.hs
@@ -0,0 +1,23 @@
+combos :: [a] -> [a] -> [(a,a,a)]
+combos fl sl = [(c1, v, c2) | c1 <- fl, v <- sl, c2 <- fl]
+
+
+--a
+stops = "pbtdkg"
+vowels = "aeiou"
+partA = combos stops vowels
+
+--b
+partB = [(c1, a, c2) | (c1, a, c2) <- (combos stops vowels), (c1 == 'p')]
+
+--c
+nouns = ["Kevin", "Linus", "table", "rock"]
+verbs = ["eat", "build"]
+
+partC = combos nouns verbs
+
+main :: IO ()
+main = do
+  print partA
+  print partB
+  print partC
