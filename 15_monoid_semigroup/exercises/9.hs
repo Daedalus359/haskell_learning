2f11d0865dd983e3330ab31ab3f9df88d526217c
Kevin Bradner <kbradner112@gmail.com>
2019-04-07 19:47:17 -0700

    did I actuallly remove them this time

--- 15_monoid_semigroup/exercises/9.hs
+++
@@ -1,9 +0,0 @@
-import Test.QuickCheck
-import Data.Semigroup
-
-data Combine a b = Combine {unCombine :: (a -> b)}
-
-instance (Semigroup b) => Semigroup (Combine a b) where
-  (Combine f) <> (Combine g) = Combine (\a -> ((f a) <> (g a)))
-
---skipping QuickCheck since it seems to require more advanced tools than I have been taught
