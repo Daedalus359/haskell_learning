168778f10539ac51adf3dbc19cf4a873ff626c49
Kevin Bradner <kbradner112@gmail.com>
2019-05-14 17:26:43 -0700

    end of Monad exercises

--- 18_Monad/exercises.hs
+++ 18_Monad/exercises.hs
@@ -136,3 +136,27 @@ test4 = do
 --Section 2: writing functions
 
 --1: seems to be join
+j :: Monad m => m (m a) -> m a
+j = (\m -> m >>= id)
+
+--2:
+l1 :: Monad m => (a -> b) -> m a -> m b
+l1 = (<*>) . return
+
+--3:
+l2 :: Monad m => (a -> b -> c) -> m a -> m b -> m c
+l2 f ma= (<*>) $ (<*>) (return f) ma
+
+--4
+a :: Monad m => m a -> m (a -> b) -> m b
+a = flip (<*>)
+
+--5
+meh :: Monad m => [a] -> (a -> m b) -> m [b]
+meh [] _ = return []
+meh (x : xs) f = l2 (:) (f x) (meh xs f)
+
+--6
+flipType :: Monad m => [m a] -> m [a]
+flipType lst= meh lst id
+
