e60fb06600bdaa0cab576a909e8ef35560f9e118
Kevin Bradner <kbradner112@gmail.com>
2019-06-08 22:05:42 -0700

    end of State

--- 23_state/exercises.hs
+++ 23_state/exercises.hs
@@ -1,14 +1,23 @@
 module Exercises where
 
-import Control.Monad
-import Control.Monad.Trans.State
+newtype State s a = State {runState :: s -> (a, s)}
 
 --1
-myGet :: State s s
-myGet = state (\s -> (s, s))
+get :: State s s
+get = State (\s -> (s, s))
 
 --2
-myPut :: s -> State s ()
-myPut s = state (\x -> ((), s))
+put :: s -> State s ()
+put s = State (\x -> ((), s))
 
+--3
+exec :: State s a -> s -> s
+exec (State sa) s = snd $ sa s
 
+--4
+eval :: State s a -> s -> a
+eval st@(State sa) s = fst $ runState st s
+
+--5
+modify :: (s -> s) -> State s ()
+modify fss = State (\s -> ((), fss s))
