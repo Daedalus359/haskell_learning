48a9f70e28c73aab647017a2291594d60ac2194a
= <=>
2019-07-09 12:01:57 -0700

    more parser combinator exercises

--- 24_parser_combinators/Exercises.hs
+++ 24_parser_combinators/Exercises.hs
@@ -62,4 +62,64 @@ main1 = do
   print $ parseString parseSemVer mempty "1.2.3--5"
   print $ parseString parseSemVer mempty "1.2.3-4-"
 
---Ex2
+--Ex4
+type AreaCode = Int
+type Exchange = Int
+type LineNumber = Int
+
+data PhoneNumber = PhoneNumber AreaCode Exchange LineNumber
+  deriving (Eq, Show)
+
+parsePhone :: Parser PhoneNumber
+parsePhone = do
+  --get rid of the single digit leading number, if present
+  _ <- optional $ try $ digit >> choice [char '-', char ' ']
+  ac <- optional (char '(') *> (count 3 digit) <* (optional (char ')')) <* (optional (choice $ fmap char ['-', ' ']))--ac :: [char]
+  exch <- (count 3 digit) <* (optional (choice $ fmap char ['-', ' ']))
+  linNum <- (count 4 digit)
+  return $ PhoneNumber (read ac) (read exch) (read linNum)
+
+main4 :: IO ()
+main4 = do
+  let p s = print $ parseString parsePhone mempty s
+  p "123-456-7890"
+  p "1234567890"
+  p "(123) 456-7890"
+  p "1-123-456-7890"
+
+--Ex5
+type Year = Integer
+type Month = Integer
+type Day = Integer
+
+data Date = Date Year Month Day
+  deriving (Show)
+
+parseDate :: Parser Date
+parseDate = do
+  year <- string "# " *> natural <* (char '-')
+  month <- natural <* (char '-')
+  day <- natural
+  return $ Date year month day
+
+type Hour = Integer
+type Minute = Integer
+data Time = Time Hour Minute
+  deriving (Show)
+
+data Activity = Activity Time String
+  deriving (Show)
+
+--modify to ignore trailing comments
+parseActivity :: Parser Activity
+parseActivity = do
+  hour <- natural <* (char ':')
+  minute <- natural <* spaces
+  actString <- some anyChar
+  return $ Activity (Time hour minute) actString
+
+main5 :: IO ()
+main5 = do
+  let ps p s = print $ parseString p mempty s
+  ps parseDate "# 2025-02-05"
+  ps parseActivity "09:00 Sanitizing moisture collector"
