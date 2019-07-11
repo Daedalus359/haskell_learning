f7ef140713e0d0e6f4787043e3804b73a36429fc
= <=>
2019-07-11 16:04:25 -0700

    ReaderT Functor instance compiles now

--- 26_monad_transformers/ReaderT.hs
+++ 26_monad_transformers/ReaderT.hs
@@ -1,7 +1,7 @@
 module ReaderT where
 
-newtype ReaderT r m a where
+newtype ReaderT r m a =
   ReaderT {runReaderT :: r -> m a}
 
 instance Functor m => Functor (ReaderT r m) where
-  fmap f2 (ReaderT f1) = (fmap . fmap) f2 f1
+  fmap f2 (ReaderT f1) = ReaderT $ (fmap . fmap) f2 f1
