ac03adf2b51f221f3b99232c2ab400aa892a03df
= <=>
2019-07-17 14:18:08 -0700

    added all of the state stuff to Morra, close enough

--- 26_monad_transformers/Morra.hs
+++ 26_monad_transformers/Morra.hs
@@ -1,5 +1,7 @@
 module Morra where
 
+import StateT
+
 import Text.Trifecta
 import System.Random
 
@@ -7,11 +9,34 @@ a = "test"
 
 data Winner = Player1 | Player2
 
+data GameState = GameState
+  { p13Grams :: [(Integer, Integer, Integer)]
+  , p1Last2Moves :: (Integer, Integer)
+  , getGen :: StdGen
+  }
+
+newGameState :: StdGen -> GameState
+newGameState gen = GameState [] (0, 0) gen--just a hack, assume player only ever does 1 and 2
+
+recMove :: Integer -> StdGen -> GameState -> GameState
+recMove i newGen (GameState p13g p1l2m gen) =
+  GameState (threeGram : p13g) cycledMoves newGen
+  where
+    threeGram = (\l -> \(j, k) -> (j, k, l)) i p1l2m
+    cycledMoves = (\(j, k, l) -> (k, l)) threeGram
+--runGame :: StateT GameState IO Winner
+
 main :: IO ()
-main = do
-  gen <- newStdGen
-  putStrLn "Morra vs AI"
-  playAI gen
+main =
+  do
+    gen <- newStdGen
+    putStrLn "Morra vs AI"
+    continueGame $ newGameState gen
+
+continueGame :: GameState -> IO ()
+continueGame gs = do
+  (i, newGen) <- playRoundAI $ getGen gs
+  continueGame $ recMove i newGen gs
 
 randomPrInt :: StdGen -> IO (Integer, StdGen) --add a stdGen parameter?
 randomPrInt gen =
@@ -20,14 +45,17 @@ randomPrInt gen =
     return (toInteger i, g2)
   where (i, g2) = next gen
 
+smartMove :: GameState -> IO ()
+smartMove = undefined
+
 announceWinner :: Integer -> Integer -> IO Winner --
-announceWinner i1 i2 = do
+announceWinner i1 i2 =
   if (even (i1 + i2))
     then putStrLn "C wins" >> return Player2
     else putStrLn "P wins" >> return Player1
 
-playAI :: StdGen -> IO ()
-playAI gen = do
+playRoundAI :: StdGen -> IO (Integer, StdGen)
+playRoundAI gen = do
   putStrLn "p is Player"
   putStrLn "c is Computer"
   print "P: "
@@ -35,8 +63,8 @@ playAI gen = do
   case playerResultInt of
     Failure _ -> do
       putStrLn "parse error on input, restarting"
-      playAI gen
+      playRoundAI gen
     Success i -> do
       (compInt, nextGen) <- randomPrInt gen
       _ <- announceWinner i compInt
-      return ()
+      return (i, nextGen)
